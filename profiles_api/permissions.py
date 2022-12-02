a804d644620720bc95b170e2a9eb63d20b64d1b1
kkkMaks <kurchenko.maks.kpi@gmail.com>
2022-12-02 21:10:20 +0200

    Added searching to user profile API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
-
+    #SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
         if request.method in permissions.SAFE_METHODS:
